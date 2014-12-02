000f0479a8e773004a62aff5440641c53f09c904
Mike Shea <michael.shea@salesforce.com>
2014-12-02 08:49:21 -0500

    Use bash -l in run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -o nounset
 if [ "$#" -eq 0 ]; then
   export PORT=80
   cd /cowsapi
-  exec bundle exec bin/cowsapi 
+  exec bash -l -c "bundle exec bin/cowsapi"
 else
   if [ "$1" = "--lol" ]; then
     /usr/bin/cowsay ${@:2:$#} | lolcat -f
